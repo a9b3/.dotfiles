80858ee80881d8d91276b470a4a0edc00e0ef8db
Sam Lau <esayemm@gmail.com>
2021-10-14 15:19:50 -0600

    [provision] feat: add ssh pub key

--- provision_linux.sh
+++ provision_linux.sh
@@ -1,28 +1,38 @@
 # Run with the following command
 #
-#   ssh -t user@server 'bash -s' <(wget -qO- https://raw.githubusercontent.com/esayemm/dotfiles/master/provision.sh)
+#   ssh -t root@137.184.127.181 'bash -s' <(wget -qO- https://raw.githubusercontent.com/a9b3/.dotfiles/master/provision_linux.sh)
+#
+# or ssh in as root and run
+#
+#   bash <(curl -s https://raw.githubusercontent.com/a9b3/.dotfiles/master/provision_linux.sh)
 #
 
 echo "Enter name"
-
 read create_user
 
 # Create user
 adduser $create_user
 usermod -aG sudo $create_user
-sudo passwd $create_user
-
-# Allow ssh
-ufw allow OpenSSH
-ufw enable
+# sudo passwd $create_user
 
 # Move ssh keys
 rsync --archive --chown=$create_user:$create_user ~/.ssh /home/$create_user
 
+sudo chsh -s $(which zsh)
+
 # Assume created user
 su - $create_user
 
-sudo chsh -s $(which zsh)
+echo "Enter email"
+read create_user_email
+
+ssh-keygen -t rsa -b 4096 -C "$create_user_email"
+
+echo "Copy the public key and add to https://github.com/settings/ssh/new"
+echo ""
+cat $HOME/.ssh/*.pub
+echo ""
+read -n 1 -s -r -p "Press any key to continue"
 
 # Provision with dotfiles and home-manager
 git clone git@github.com:esayemm/.dotfiles.git $HOME/.dotfiles
