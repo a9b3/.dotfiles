a600d1898307e15d1b32e970843e9840c1a618c9
Sam Lau <esayemm@gmail.com>
2021-10-14 21:44:04 -0600

    [provision] fix: lazy create

--- provision_linux.sh
+++ provision_linux.sh
@@ -6,44 +6,79 @@
 #
 #   bash <(curl -s -H "Cache-Control: no-cache" https://raw.githubusercontent.com/a9b3/.dotfiles/master/provision_linux.sh)
 #
+# To remove a user run this
+#
+#   deluser newuser
+#
+
+# exit when any command fails
+set -e
 
 echo "Enter name"
 read create_user
 
-# Create user
-adduser $create_user
-usermod -aG sudo $create_user
-# sudo passwd $create_user
-
-# Move ssh keys
-rsync --archive --chown=$create_user:$create_user ~/.ssh /home/$create_user
-
-# Run as user
-sudo -i -u $create_user bash << EOF
 echo "Enter email"
 read create_user_email
+HOME_DIR="/home/$create_user"
 
-ssh-keygen -t rsa -b 4096 -C "$create_user_email"
+# Create user
+if [[ -z "$(cat /etc/passwd | grep $create_user)" ]]; then
+  adduser "$create_user"
+  usermod -aG sudo "$create_user"
+fi
 
+# Move ssh keys
+if [[ ! -f "/home/$create_user/.ssh" ]]; then
+  rsync --archive --chown="$create_user":"$create_user" ~/.ssh "/home/$create_user"
+fi
+
+# Create ssh key
+# Run as user
+sudo -i -u "$create_user" bash << EOF
+set -e
+
+if [[ ! -f "$HOME_DIR/.ssh/id_rsa.pub" ]]; then
+  ssh-keygen -t rsa -b 4096 -C "$create_user_email"
+fi
+
+echo ""
+echo ""
 echo "Copy the public key and add to https://github.com/settings/ssh/new"
 echo ""
-cat $HOME/.ssh/*.pub
+echo ""
+cat "$HOME_DIR/.ssh/id_rsa.pub"
+EOF
+
+echo ""
+echo ""
 echo ""
 read -n 1 -s -r -p "Press any key to continue"
+echo ""
+
+# clone dotfiles and install nix
+sudo -i -u "$create_user" bash << EOF
+set -e
 
 # Provision with dotfiles and home-manager
-git clone git@github.com:esayemm/.dotfiles.git $HOME/.dotfiles
+if [[ ! -d "$HOME_DIR/.dotfiles" ]]; then
+  git clone git@github.com:esayemm/.dotfiles.git "$HOME_DIR/.dotfiles"
+fi
 
-curl -L https://nixos.org/nix/install | sh
+if ! which nix &> /dev/null; then
+  curl -L https://nixos.org/nix/install | sh -s -- --daemon
+fi
+EOF
 
-. $HOME/.nix-profile/etc/profile.d/nix.sh
+# install home-manager
+sudo -i -u "$create_user" bash << EOF
+set -e
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
-rm $HOME/.config/nixpkgs/home.nix
-ln -s $HOME/.dotfiles/home.nix $HOME/.config/nixpkgs/home.nix
+rm "$HOME_DIR/.config/nixpkgs/home.nix"
+ln -s "$HOME_DIR/.dotfiles/home.nix" "$HOME_DIR/.config/nixpkgs/home.nix"
 
 home-manager switch
 
