40b48b0483cc7e5ce1226f28f496a4a9fdec7520
Sam Lau <esayemm@gmail.com>
2024-03-14 00:49:05 -0700

    feat: add vscode style cmp

--- vim/lua/plugins/cmp.lua
+++ vim/lua/plugins/cmp.lua
@@ -1,5 +1,59 @@
 return {
 	{
+		"onsails/lspkind.nvim",
+		config = function()
+			require("lspkind").init({
+				-- DEPRECATED (use mode instead): enables text annotations
+				--
+				-- default: true
+				-- with_text = true,
+
+				-- defines how annotations are shown
+				-- default: symbol
+				-- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+				mode = "symbol_text",
+
+				-- default symbol map
+				-- can be either 'default' (requires nerd-fonts font) or
+				-- 'codicons' for codicon preset (requires vscode-codicons font)
+				--
+				-- default: 'default'
+				preset = "codicons",
+
+				-- override preset symbols
+				--
+				-- default: {}
+				symbol_map = {
+					Text = "󰉿",
+					Method = "󰆧",
+					Function = "󰊕",
+					Constructor = "",
+					Field = "󰜢",
+					Variable = "󰀫",
+					Class = "󰠱",
+					Interface = "",
+					Module = "",
+					Property = "󰜢",
+					Unit = "󰑭",
+					Value = "󰎠",
+					Enum = "",
+					Keyword = "󰌋",
+					Snippet = "",
+					Color = "󰏘",
+					File = "󰈙",
+					Reference = "󰈇",
+					Folder = "󰉋",
+					EnumMember = "",
+					Constant = "󰏿",
+					Struct = "󰙅",
+					Event = "",
+					Operator = "󰆕",
+					TypeParameter = "",
+				},
+			})
+		end,
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
 		dependencies = {
@@ -9,14 +63,17 @@ return {
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
+			"onsails/lspkind.nvim",
 		},
 		config = function()
 			local luasnip = require("luasnip")
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
 
 			require("luasnip/loaders/from_vscode").load()
 			require("luasnip/loaders/from_snipmate").load({ path = { "./snippets" } })
 
-			require("cmp").setup({
+			cmp.setup({
 				completion = {
 					completeopt = "menu,menuone,noinsert",
 				},
@@ -26,18 +83,18 @@ return {
 					end,
 				},
 				mapping = {
-					["<S-Tab>"] = require("cmp").mapping.select_prev_item(),
-					["<Tab>"] = require("cmp").mapping.select_next_item(),
-					["<C-h>"] = require("cmp").mapping.scroll_docs(-4),
-					["<C-l>"] = require("cmp").mapping.scroll_docs(4),
-					["<C-Space>"] = require("cmp").mapping.complete(),
-					["<C-d>"] = require("cmp").mapping.close(),
-					["<C-k>"] = require("cmp").mapping.confirm({
-						behavior = require("cmp").ConfirmBehavior.Insert,
+					["<S-Tab>"] = cmp.mapping.select_prev_item(),
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<C-h>"] = cmp.mapping.scroll_docs(-4),
+					["<C-l>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-d>"] = cmp.mapping.close(),
+					["<C-k>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
 						select = true,
 					}),
-					["<CR>"] = require("cmp").mapping.confirm({
-						behavior = require("cmp").ConfirmBehavior.Insert,
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
 						select = true,
 					}),
 				},
@@ -47,10 +104,35 @@ return {
 					{ name = "buffer" },
 					{ name = "path" },
 				},
+
+				window = {
+					completion = {
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+						col_offset = -3,
+						side_padding = 0,
+					},
+					documentation = {
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+						col_offset = 0,
+						side = "right",
+					},
+				},
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						local kind =
+							require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. (strings[1] or "") .. " "
+						kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+						return kind
+					end,
+				},
 			})
 
 			-- setup filetype extends here
-			require("luasnip").filetype_extend("typescript", { "javascript" })
+			luasnip.filetype_extend("typescript", { "javascript" })
 		end,
 	},
 }
