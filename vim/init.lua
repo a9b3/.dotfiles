3d337a9ea19888cec91ba11b968a36ac232f6f23
Sam Lau <esayemm@gmail.com>
2025-07-25 23:04:07 -0700

    move lazy init code to its own file

--- vim/init.lua
+++ vim/init.lua
@@ -1,49 +1,8 @@
--- lazy.nvim setup
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = "," -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
-vim.opt.sessionoptions = { "buffers", "tabpages", "globals" } -- used by scope.nvim needs to happen before plugin loads
-
--- configure plugins
-local plugins = {}
 local hpath = os.getenv("HOME")
+local customPath = hpath .. "/.dotfiles/vim/lua/?.lua"
+package.path = package.path ..";" .. customPath
 
-package.preload.navPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/nav.lua")
-vim.list_extend(plugins, require("navPlugins"))
-package.preload.editingPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/editing.lua")
-vim.list_extend(plugins, require("editingPlugins"))
-package.preload.uiPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/ui.lua")
-vim.list_extend(plugins, require("uiPlugins"))
-package.preload.telescopePlugins = loadfile("/Users/es/.dotfiles/vim/lua/plugins/telescope.lua")
-vim.list_extend(plugins, require("telescopePlugins"))
-package.preload.lspPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/lsp.lua")
-vim.list_extend(plugins, require("lspPlugins"))
-package.preload.cmpPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/cmp.lua")
-vim.list_extend(plugins, require("cmpPlugins"))
-package.preload.formatterPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/formatter.lua")
-vim.list_extend(plugins, require("formatterPlugins"))
-package.preload.linterPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/linter.lua")
-vim.list_extend(plugins, require("linterPlugins"))
-package.preload.gitPlugins = loadfile(hpath .. "/.dotfiles/vim/lua/plugins/git.lua")
-vim.list_extend(plugins, require("gitPlugins"))
-
-require("lazy").setup(plugins, {
-	ui = {
-		border = "single",
-	},
-})
+require("confs.lazy")
 
 package.preload.basicConf = loadfile(hpath .. "/.dotfiles/vim/lua/confs/basic.lua")
 require("basicConf")
