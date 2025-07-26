21bb454ec9a6cc2c4c5c2ffa95797a3a96993060
Sam Lau <esayemm@gmail.com>
2025-07-26 16:04:32 -0700

    fix comment keybinding for outside tmux

--- vim/lua/plugins/editing.lua
+++ vim/lua/plugins/editing.lua
@@ -20,6 +20,12 @@ return {
 				line = "<C-_>",
 			},
 		},
+		config = function(_, opts)
+			require("Comment").setup(opts)
+			-- Neovim outside of tmux is getting <C-/> while inside tmux it gets <C-_>
+			vim.keymap.set("n", "<C-/>", "<Plug>(comment_toggle_linewise_current)", { noremap = false, silent = true })
+			vim.keymap.set("x", "<C-/>", "<Plug>(comment_toggle_linewise_visual)", { noremap = false, silent = true })
+		end,
 	},
 	{
 		"windwp/nvim-autopairs",
