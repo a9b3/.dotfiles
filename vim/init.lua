4d190e31663135fdfbb920c75f28ce7568fe81fd
Sam Lau <esayemm@gmail.com>
2025-07-25 23:06:41 -0700

    use package.path instead of preloads

--- vim/init.lua
+++ vim/init.lua
@@ -1,14 +1,10 @@
 local hpath = os.getenv("HOME")
 local customPath = hpath .. "/.dotfiles/vim/lua/?.lua"
-package.path = package.path ..";" .. customPath
+package.path = package.path .. ";" .. customPath
 
 require("confs.lazy")
 
-package.preload.basicConf = loadfile(hpath .. "/.dotfiles/vim/lua/confs/basic.lua")
-require("basicConf")
-package.preload.autocmdConf = loadfile(hpath .. "/.dotfiles/vim/lua/confs/autocmd.lua")
-require("autocmdConf")
-package.preload.pluginsConf = loadfile(hpath .. "/.dotfiles/vim/lua/confs/plugins.lua")
-require("pluginsConf")
-package.preload.keymapsConf = loadfile(hpath .. "/.dotfiles/vim/lua/confs/keymaps.lua")
-require("keymapsConf")
+require("confs.basic")
+require("confs.autocmd")
+require("confs.plugins")
+require("confs.keymaps")
